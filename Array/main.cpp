037a69ecfec429cf44b6f737bb74b9b3bf1e74fb
jdr14 <jdr14@vt.edu>
2023-12-08 15:40:39 -0500

    Initial Commit

---
+++ Array/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Array
+//
+//  Created by Joseph Rodgers on 12/8/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
